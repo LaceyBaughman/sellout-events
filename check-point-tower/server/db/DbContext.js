1058320c4b2d3ef1b48be52e99bc0ab9a3447d98
Lacey Baughman <laceyebaughman@gmail.com>
2022-03-18 13:55:21 -0600

    envs

--- check-point-tower/server/db/DbContext.js
+++ check-point-tower/server/db/DbContext.js
@@ -6,6 +6,10 @@ class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Events = mongoose.model('Event', EventSchema);
+  Tickets = mongoose.model('Ticket', TicketSchema);
+
+  Comments = mongoose.model('Comment', CommentSchema);
 }
 
 export const dbContext = new DbContext()
